fd6ed00b0d00be6a1a5cffa77cbe7d1a30d5ac0d
nitishmayekar <nitishmayekar333@gmail.com>
2023-11-13 21:44:31 -0500

    Created Weather API End point

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { weatherApi} from "./apis/index.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/location/:searchTerm", async (req, res) => {
+  const { searchTerm } = req.params;
+  const data = await weatherApi.get(
+    `geo/1.0/direct?q=${searchTerm}&limit=5&appid=${process.env.OPEN_WEATHER_API_KEY}`
+  );
+
+  res.send(data.data);
+});
+
+app.get("/weather/:lat/:lon", async (req, res) => {
+  const { lat, lon } = req.params;
+  const data = await weatherApi.get(
+    `data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.OPEN_WEATHER_API_KEY}`
+  );
+  res.send(data.data);
+});
+
+app.listen(PORT, () => {
+  console.log("Server listening on port 5000");
+});
